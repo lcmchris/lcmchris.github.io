8716f98990e37f4420a4d0a4b7cfee25526879fd
Chris Lai <lcmchristopher@gmail.com>
2024-10-23 10:19:25 +0100

    Fixing layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
   <h2>
     I like creating things. This is a log of all things I create and muse.
   </h2>
-  <div class="filterBar" style="display: flex;flex-direction:row">
+  <div class="filterBar" style="display: flex;flex-direction:row; gap:10px">
     {#each data.allTags as tags}
       <Button on:click={tagFilter(tags)} selected>
         <h3>{tags}</h3>
@@ -28,7 +28,7 @@
   </div>
 
   {#each data.allPosts as post}
-    {#if filter.includes(post.tags)}
+    {#if !filter.includes(post.tags)}
       <div style="display: flex; gap:10px;">
         <h4>{post.tags}</h4>
 
