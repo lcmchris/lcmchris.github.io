a8526e3565db831d11c1e8cb419d934faad9feaa
Chris Lai <lcmchristopher@gmail.com>
2025-12-10 22:36:06 +0000

    Updates

---
+++ _app/immutable/chunks/Hp6ZSQdV.js
@@ -0,0 +1,9 @@
+import{f as o,a}from"./NeLHWg22.js";import"./BASVQn1p.js";import{v as i}from"./BBWcagcT.js";const r={title:"Beyond time",tags:"Writing",first:"2025-12-05T00:00:00.000Z",last:"2025-12-05T00:00:00.000Z",uuid:"8dbc4608-1103-4442-b82a-99b3ac432bf7"},{title:m,tags:d,first:u,last:p,uuid:b}=r;var s=o(`<p>Angelo is swimming is a lido.</p> <blockquote><blockquote><p>Teleportation device
+Discombopulation and reconstruction.
+Daily usage and is common. What happens to the organic matter left behind?
+Travelling the universe
+Human backups
+how are memories stored in our brains
+Actual cloning is prohibited. Due to the widespread abuse of labour and slavery.
+Teleporter is a 3d biochemical printer
+Reporter scandal on constructed memorys ‘implanted ads’</p></blockquote></blockquote>`,1);function f(t){var e=s();i(2),a(t,e)}export{f as default,r as metadata};
