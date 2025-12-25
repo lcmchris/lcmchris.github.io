f90457a70ad7476f3a137e7c53e9b477809b95f2
Chris Lai <lcmchristopher@gmail.com>
2025-12-25 16:45:08 +0000

    Updates

---
+++ _app/immutable/chunks/BPPDLFZA.js
@@ -0,0 +1,4 @@
+import{f as e,a as i}from"./Bx5urKML.js";import"./DbovNYc0.js";import{v as a}from"./CzYMjeXN.js";const o={title:"List of potential business ideas",tags:"Projects",first:"2025-12-07T00:00:00.000Z",last:"2025-12-07T00:00:00.000Z",uuid:"61002295-567a-4a01-b176-6679310ac99a"},{title:f,tags:p,first:u,last:d,uuid:g}=o;var n=e(`<p>I have yet to give up on my startup dreams.
+Here’s a list of potential business ideas for this season.</p> <h3><em>Tried</em></h3> <p>Meeting optimiser
+AI based Smart homes
+AI Font generation</p> <h3>Potential</h3> <ul><li>Booking platform for Dentist</li> <li>Vivino for coffee snobs</li> <li>SOPs management system</li> <li></li></ul> <p>TBC</p>`,1);function h(t){var s=n();a(10),i(t,s)}export{h as default,o as metadata};
